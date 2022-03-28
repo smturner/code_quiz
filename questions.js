606374e54f6a0e7142b99d76ca38a72adf5ccbfa
Sarah Turner <turnersarah228@gmail.com>
2022-03-27 21:20:36 -0500

    added startQuiz function and linked it to the button, unlinked the timer for now

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions=[
+{
+    questionText: "Commonly used data types DO NOT include:",
+    answerChoices: ["stings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+    
+},
+console.log ("title")
+]
+
+// {
+//     title: "The condition in an if/else statement is enclosed within _______.",
+//     choices: [ ]
+// }
+// ]
